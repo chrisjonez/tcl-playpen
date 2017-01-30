0d6169f5d2552912f47da969210faf0a324dd53b
chrisjonez <chrisjonez@thayerconsulting.co.uk>
2017-01-30 13:49:20 +0000

    Coin denomination now checked using binary search.

--- tcl-change/src/main/java/tcl/problems/change/Change.java
+++ tcl-change/src/main/java/tcl/problems/change/Change.java
@@ -2,30 +2,30 @@ package tcl.problems.change;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Change {
 
-	List<Integer> dens;
+	private int[] dens;
 	
 	public Change(int[] dens){		
-		this.dens = Arrays.stream(dens).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-		if (this.dens.get(dens.length-1) < 0)
-			throw new IllegalArgumentException("Denominations cannot be negative: " + this.dens.get(dens.length-1));	
+		Arrays.sort(dens);
+		this.dens = dens;
+		
+		int minDens = this.dens[0];
+		if (minDens < 0)
+			throw new IllegalArgumentException("Denominations cannot be negative: " + minDens);	
 	}
 	
-	private void checkDenoms(List<Integer> coins){		
+	private void checkDenoms(List<Integer> coins){
 		for (int coin : coins){
-			if (!dens.contains(coin))
-				throw new IllegalArgumentException("Coin: " + coin + " not allowed!");
+			if (Arrays.binarySearch(dens, coin)<0)
+				throw new IllegalArgumentException("Coin: " + coin + " not allowed!");							
 		}
 	}
 	
 	public static int sum(List<Integer> coins){
-		return coins.stream().mapToInt(Integer::intValue).sum();
-		
+		return coins.stream().mapToInt(Integer::intValue).sum();		
 	}
 	
 	public List<Integer> pay(int amount, List<Integer> coins){
@@ -38,18 +38,18 @@ public class Change {
 			throw new IllegalArgumentException("Not enough!");
 		
 		int changeRequired = sum - amount;
-		int denIndex = 0;
+		int denIndex = dens.length-1;
 		ArrayList<Integer> change = new ArrayList<Integer>();
 		
 		while (changeRequired>0){
-			int currDen = dens.get(denIndex);
+			int currDen = dens[denIndex];
 			changeRequired = changeRequired - currDen;
 			if (changeRequired==0){
 				change.add(currDen);
 				break;
 			}else if (changeRequired<0){
 				changeRequired += currDen;
-				denIndex++;
+				--denIndex;
 			}else{
 				change.add(currDen);
 			}
